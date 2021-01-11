9922518d8a5a740c16c67fd93107a2f2cb4a75c0
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-01-11 19:44:22 +0100

    Add file type validation, author and title length validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -11,11 +11,12 @@ exports.add = async (req, res) => {
     if(title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if (fileExt === '.jpeg' || '.png' || '.gif' && title.length <= 25 && author.length <= 50) {
       const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
       await newPhoto.save(); // ...save new photo in DB
       res.json(newPhoto);
-      
+      }
     } else {
       throw new Error('Wrong input!');
     }
